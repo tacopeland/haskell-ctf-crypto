fae25775c3720070edf05e28b1a14fad46a2e3c6
Timothy Copeland <timcopeland1111@gmail.com>
2022-06-01 22:42:47 +1000

    Update encoding helpers

--- src/Crypto/Util/Encoding.hs
+++ src/Crypto/Util/Encoding.hs
@@ -1,19 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Crypto.Util.Encoding where
 
-import Data.ByteString as B
-import Data.ByteString.Char8 as C
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 import Data.Char
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Numeric (readHex, showHex)
 
-bytesToInteger :: ByteString -> Integer
+-- |This can fail if not given a string of hex digits.
+hexToInteger :: T.Text -> Integer
+hexToInteger hex = fst (head (readHex (T.unpack hex)))
+
+bytesToInteger :: B.ByteString -> Integer
 bytesToInteger = B.foldl (\x y -> 256 * x + toInteger y) 0
 
-strToInteger :: String -> Integer
-strToInteger = bytesToInteger . C.pack
+textToInteger :: T.Text -> Integer
+textToInteger = bytesToInteger . T.encodeUtf8
 
-integerToBytes :: Integer -> ByteString
+integerToHex :: Integer -> T.Text
+integerToHex i = T.pack (showHex i "")
+
+integerToBytes :: Integer -> B.ByteString
 integerToBytes n =
     let inner n
           | n == 0 = []
           | otherwise = inner (n `div` 256) ++ [chr (fromInteger (n `mod` 256))]
      in C.pack (inner n)
+
+integerToText = T.decodeUtf8 . integerToBytes
